cb2758b6e1f32dce6297789fadc8e964a90c4bd1
Daniel Ciccotosto-Camp <DanielCiccotostoCamp@nti.com.au>
2023-08-12 17:46:08 +1000

    PRO-17: updated documentation

--- GPT_test.py
+++ GPT_test.py
@@ -13,6 +13,7 @@ response = openai.ChatCompletion.create(
   temperature=0.7
 )
 print(response['choices'][0]['message']['content'])
-# print(run_conversation())
 
 # https://github.com/openai/openai-cookbook/blob/main/examples/How_to_format_inputs_to_ChatGPT_models.ipynb I'm just going to leave this here for now
+# but it is a good thing for the BA's to look at when they begin testing the GPT's functionality - the prompt can be more of a conversation
+# to potentially prompt it to say the right thing
