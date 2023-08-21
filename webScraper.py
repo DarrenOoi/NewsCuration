7bbaec06cf352a59bcb042abe0aff9f00c46c538
Ron-Ash <ashri.ron@gmail.com>
2023-08-21 19:29:16 +1000

    basic paywall handling (log error and exit)

--- webScraper.py
+++ webScraper.py
@@ -2,25 +2,45 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-def check_accessible(pageToScrape):
-    # needs to check if isAccessibleForFree:"False" exists when inspected
-    return True
+class PageValidator:
+    def __init__(self, pageToScrape) -> None:
+        self.pageToScrape = pageToScrape
+
+    def isStatusSuccessful(self): # Need to handle redirect message
+        status = self.pageToScrape.status_code
+        if status >= 200 and status < 300:
+            return True
+        return False
+
+    def isAccessibleForFree(self):
+        match = re.search(r'"isAccessibleForFree"\:"?(\w+)"?', self.pageToScrape.text)
+
+        if match and match.group(1).lower() == "false":
+            return False
+        return True
+
+    def isArticleAccessible(self):
+        if self.isStatusSuccessful() and self.isAccessibleForFree():
+            return True
+        return False
 
 def articleScraper(url):
     pageToScrape = requests.get(url)
-    soup = BeautifulSoup(pageToScrape.text, "html.parser")
 
-    if check_accessible(pageToScrape):
-        paragraphsSoup = soup.findAll("p")
-        paragraphs = [i.text for i in paragraphsSoup]
-        
-        dirtyArticle = "\n".join(paragraphs)
-        whiteSpaceFilledArticle = re.sub("[\n\t]", " ", dirtyArticle)
-        cleanArticle = re.sub(" +", " ", whiteSpaceFilledArticle)
-        return cleanArticle
-    else:
+    validator = PageValidator(pageToScrape)
+    if not validator.isArticleAccessible():
         print('Paywall detected, if paywall does not exist please use other method of input')
         return None
+
+    soup = BeautifulSoup(pageToScrape.content, "html.parser")
+
+    paragraphsSoup = soup.findAll("p")
+    paragraphs = [i.text for i in paragraphsSoup]
+    
+    dirtyArticle = "\n".join(paragraphs)
+    whiteSpaceFilledArticle = re.sub("[\n\t]", " ", dirtyArticle)
+    cleanArticle = re.sub(" +", " ", whiteSpaceFilledArticle)
+    return cleanArticle
     
 if __name__ == '__main__':
     print(articleScraper(input("Enter URL: \n")))
