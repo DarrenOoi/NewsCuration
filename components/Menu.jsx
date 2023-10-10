4bdd1996aaaa868c2173db78fa996d40835959d7
Manav Trivedi <trivedimanav2003@gmail.com>
2023-10-10 15:18:18 +1000

    updated profile search page

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,4 +1,15 @@
+import { useRouter } from 'next/router';
+
 const Menu = ({currentPage}) => {
+    const router = useRouter();
+
+    const articleClick = () => {
+        router.push('/articleSearch');
+    };
+
+    const profileClick = () => {
+        router.push('/profileSearch');
+    };
 
     return (
         <div className = "bg-[#5F7A95] flex flex-row space-x-32 pb-8">
@@ -7,10 +18,12 @@ const Menu = ({currentPage}) => {
             </div>
             <div className = "mt-14 flex flex-col">
                 <button 
+                  onClick={articleClick}
                   className = {currentPage == "article" ? "btn btn-ghost btn-sm text-[#FFB039] text-lg font-bold" : "btn btn-ghost btn-sm text-[#7895B1] text-lg font-bold"}>
                     ARTICLE SEARCH
                 </button>
                 <button 
+                  onClick={profileClick}
                   className = {currentPage == "profile" ? "btn btn-ghost btn-sm text-[#FFB039] text-lg font-bold" : "btn btn-ghost btn-sm text-[#7895B1] text-lg font-bold"}>
                     PROFILE SEARCH
                 </button>
