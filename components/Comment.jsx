e76203f7e245368708cf62fc272b3a8e3b1ccbe3
Manav Trivedi <trivedimanav2003@gmail.com>
2023-10-11 11:33:56 +1000

    finished visuals for the features

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -1,7 +1,18 @@
+import Image from 'next/image';
+import Pic from "./pictures/pic.png"
+
 const Comment = ({user, text}) => {
     return (
-        <div>
-
+        <div className="flex flex-row mt-7 ml-2 items-start">
+            <Image
+                src={Pic}
+                width={30}
+                height={30}
+            />
+            <div className="flex flex-col ml-4" style={{width:"944px"}}>
+                <div className="p text-xs font-semibold text-black">{user}</div>
+                <div className="p text-xs font-normal text-black">{text}</div>
+            </div>
         </div>
     );
 };
