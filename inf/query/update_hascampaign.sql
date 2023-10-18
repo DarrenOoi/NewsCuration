a3d50423587b0be01f96ea029ed128069193ad47
Daniel Ciccotosto-Camp <DanielCiccotostoCamp@nti.com.au>
2023-10-18 15:41:04 +1000

    more bug fixes

--- inf/query/update_hascampaign.sql
+++ inf/query/update_hascampaign.sql
@@ -1,8 +1,3 @@
---performed as part of --DDLFromFile import
-
-UPDATE Politician P
-SET HasCampaign = 1 WHERE EXISTS (
-    SELECT * FROM 
-    Politician_CampaignPoliciesByID PCP
-    WHERE PCP.ID_Policitian = P.ID
-)
+UPDATE Politician
+JOIN Politician_CampaignPoliciesByID PCP ON Politician.ID = PCP.ID_Politician
+SET Politician.HasCampaign = 1;
