aa095e4678779110041538238e69f973b4a72981
Daniel Ciccotosto-Camp <DanielCiccotostoCamp@nti.com.au>
2023-10-24 19:38:07 +1000

    argparse to remove all table records, may want to keep handy

---
+++ inf/query/temp.sql
@@ -0,0 +1,8 @@
+DELETE FROM Article WHERE 1=1;
+DELETE FROM Politician_KeyTable WHERE 1=1;
+DELETE FROM Politician WHERE 1=1;
+DELETE FROM Polling WHERE 1=1;
+DELETE FROM Comments WHERE 1=1;
+DELETE FROM Politician_CampaignPoliciesByID WHERE 1=1;
+DELETE FROM Article_ArticleBias WHERE 1=1;
+DELETE FROM Politician_CampaignPolicies WHERE 1=1;
